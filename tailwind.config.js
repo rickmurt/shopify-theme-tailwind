a33fd8782fbc5a81028fce9b1a1263ef3df9bf1e
Rick Murt <rickmurt@gmail.com>
2021-11-24 12:53:00 -0800

    Colors and logos and favicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#042057',
+          '50': '#E3ECFE',
+          '100': '#BAD0FC',
+          '200': '#6798F8',
+          '300': '#1460F4',
+          '400': '#083EAA',
+          '500': '#042057',
+          '600': '#031B48',
+          '700': '#03153A',
+          '800': '#02102B',
+          '900': '#010A1C'
+        },
+        gradient: {
+          'start': '#0015D6',
+          'end': '#0989FB'
+        }
+      },
+    },
   },
   variants: {
     extend: {},
